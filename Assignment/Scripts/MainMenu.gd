f432863b9fdad432fad337479ea5e7641af5fe66
gaffieroMikhael <146811746+mikgaf11@users.noreply.github.com>
2024-06-06 18:49:47 +0200

    Visual Update
    I updated the visuals with photo and added labels for the letters. Till now pretty much everything works except a few things

--- Assignment/Scripts/MainMenu.gd
+++ Assignment/Scripts/MainMenu.gd
@@ -1,14 +1,30 @@
 extends Control
 
+var button_scene_map = {
+	"StartGame1Button": "res://Game1.tscn",
+	"StartGame2Button": "res://Scene2.tscn",
+	"StartGame3Button": "res://Scene3.tscn"
+}
+
+
+func Button_pressed(button_name):
+	# Load the corresponding scene
+	var new_scene = load(button_scene_map[button_name])
+	# Change the current scene to the new scene
+	get_tree().change_scene_to(new_scene)
+
+func _ready():
+	$StartGame1Button.connect("pressed", Callable(self, "_on_start_game_1_button_pressed"))
+	$StartGame2Button.connect("pressed", Callable(self, "_on_start_game_2_button_pressedd"))
+	$StartGame3Button.connect("pressed", Callable(self, "_on_start_game_3_button_pressed"))
+
+
+
 func _on_start_game_1_button_pressed():
-	print("Start Game 1 button pressed")
-	get_tree().change_scene("res://Scenes/Game1.tscn")
+	Button_pressed("StartGame1Button")
 
 func _on_start_game_2_button_pressed():
-	print("Start Game 2 button pressed")
-	get_tree().change_scene("res://Scenes/Game2.tscn")
+	Button_pressed("StartGame2Button")
 
 func _on_start_game_3_button_pressed():
-	print("Start Game 3 button pressed")
-	get_tree().change_scene("res://Scenes/Game3.tscn")
-
+	Button_pressed("StartGame2Button")
